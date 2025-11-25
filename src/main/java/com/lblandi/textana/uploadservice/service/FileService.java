ab4ce63d72d3dfee07a674e662c000c12777fa03
Leandro Blandi <leandronblandi@gmail.com>
2025-11-25 13:57:53 -0300

    docs: added javadoc for each method

--- src/main/java/com/lblandi/textana/uploadservice/service/FileService.java
+++ src/main/java/com/lblandi/textana/uploadservice/service/FileService.java
@@ -3,6 +3,10 @@ package com.lblandi.textana.uploadservice.service;
 import com.lblandi.textana.uploadservice.response.FileUploadResponse;
 import org.springframework.web.multipart.MultipartFile;
 
+/**
+ * @author lblandi
+ * @since 2025-11-25
+ */
 public interface FileService {
 
     /**
@@ -11,5 +15,5 @@ public interface FileService {
      * @param file the file to be saved, encapsulated in a MultipartFile object
      * @return a String representing the identifier of the saved file
      */
-    public FileUploadResponse saveFile(MultipartFile file);
+    FileUploadResponse saveFile(MultipartFile file);
 }
--- src/main/java/com/lblandi/textana/uploadservice/service/QueueService.java
+++ src/main/java/com/lblandi/textana/uploadservice/service/QueueService.java
@@ -1,5 +1,15 @@
 package com.lblandi.textana.uploadservice.service;
 
+/**
+ * @author lblandi
+ * @since 2025-11-25
+ */
 public interface QueueService {
-    public void sendFileIdentifier(String fileIdentifier);
+
+    /**
+     * Sends the specified file identifier to be processed or queued for further handling.
+     *
+     * @param fileIdentifier the unique identifier of the file to be sent
+     */
+    void sendFileIdentifier(String fileIdentifier);
 }
